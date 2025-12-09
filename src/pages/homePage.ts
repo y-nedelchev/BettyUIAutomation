85003684c50bd2f3ce9f5c7f33ec6bfbe4b9e03a
dakof <yordan.nedelchev94@gmail.com>
2025-12-09 21:12:19 +0200

    added pages structure

---
+++ src/pages/homePage.ts
@@ -0,0 +1,17 @@
+import {Locator, Page} from "@playwright/test"
+
+export class HomePage {
+    private readonly page
+    readonly gameLogos: Locator
+    readonly gameTitles: Locator
+    readonly gamePlayButtons: Locator
+
+    constructor(page: Page) {
+        this.page = page
+        this.gameLogos = page.locator('.game-logo')
+        this.gameTitles = page.locator('.video-slider-controls').getByRole('heading')
+        this.gamePlayButtons = page.locator('.video-slider a')
+    }
+
+
+}
