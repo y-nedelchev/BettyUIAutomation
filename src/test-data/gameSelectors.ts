9c0ff52c3e17164c4881ac0b77030e08613c24ec
dakof <yordan.nedelchev94@gmail.com>
2025-12-12 14:02:48 +0200

    restructured and updated gameSelectors data file

--- src/test-data/gameSelectors.ts
+++ src/test-data/gameSelectors.ts
@@ -1,46 +1,53 @@
 export type GameConfig = {
     name: string
+    url: string
     selectors: GameSelectors
 }
 
-type GameSelectors = {
+export type GameSelectors = {
     playButton?: string
     clickableArea?: string
     balance: string
     win: string
     stakeValue: string
     spinButton: string
+    autoSpinButton: string
     spinInProgressLocator?: string
     stakeMinus: string
     stakePlus: string
+    stakeAmount: string
 }
 
 export const gameConfig: GameConfig[] =
     [
         {
         name: '10x Minimum',
+        url: 'https://tinyurl.com/3sxw6wy8',
         selectors: {
-            playButton: '.button-play-block',
+            playButton: '.button-play__inner',
             balance: '.balance .amount',
             win: '.win .amount',
             stakeValue: '.stake .amount-stake',
             spinButton: '.arrows-spin-button',
-            spinInProgressLocator: '.arrows-spin-button[disabled]',
-            stakeMinus: '[xlink:href]',
-            stakePlus: '[xlink:href]'
+            autoSpinButton: '.controls__autoplay button',
+            stakeMinus: 'button.button-stake__wrapper-minus',
+            stakePlus: 'button.button-stake__wrapper-plus',
+            stakeAmount: '.amount-stake',
+            }
+        },
+        {
+        name: 'Irish Wilds',
+        url: 'https://tinyurl.com/39hjjjf3',
+        selectors: {
+            playButton: 'button.button__slider-play',
+            balance: '.balance .amount',
+            win: '.win .amount',
+            stakeValue: '.stake .amount-stake',
+            spinButton: '.arrows-spin-button',
+            autoSpinButton: '.controls__autoplay button',
+            stakeMinus: 'button.button-stake__wrapper-minus',
+            stakePlus: 'button.button-stake__wrapper-plus',
+            stakeAmount: '.stake .amount',
         }
     },
-
-    // export const irishWildsConfig: GameConfig = {
-    //     name: 'Irish Wilds',
-    //     selectors: {
-    //         playButton: 'button:has-text("Play")',
-    //         clickableArea: 'canvas',
-    //         balance: 'TODO-balance-locator',
-    //         win: 'TODO-win-locator',
-    //         stakeValue: 'TODO-stake-value-locator',
-    //         spinButton: 'TODO-spin-button-locator',
-    //         spinInProgressLocator: 'TODO-spinning-state-locator'
-    //     }
-    // }
 ]
